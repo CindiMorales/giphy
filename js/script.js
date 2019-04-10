dc9920e06041539904e46a7f79a71347812bb326
Cindi Morales <redheadcindy123@gmail.com>
2019-04-10 22:07:05 +0000

    finish

--- js/script.js
+++ js/script.js
@@ -4,7 +4,26 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+ var search = $("input").val();
+  $.ajax({
+      url:"https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=cVRNyx7QJZX0F6nTWQ0XbssO1yb4KtSM",
+      method:"GET",
+      success:function(response){
+          
+          $(".container1").empty();
+          
+         for(var i=0; i<25; i=i+1){
+          var pickurl = response.data[i].images.original.url;
+          $('.container1').append('<img src='+ pickurl+'>');
+         }
+      
+     
+      }
+      
+      
+      
+      
+  })
   
   
 });
